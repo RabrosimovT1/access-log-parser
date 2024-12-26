83750f1cfc5732622244d1edce7f935badebc026
RAbrosimov <rabrosimov@inno.tech>
2024-12-26 12:33:39 +0300

    Изменил основную функцию программы - теперь программа производит стандартные математические действия с 2мя числами

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +text.length());
+        System.out.println("Введите число a: ");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Введите число b: ");
+        int b = new Scanner(System.in).nextInt();
+        //Сумма чисел
+        int s = a+b;
+        System.out.println("a+b= " + s);
+        //Разность чисел
+        int r = a-b;
+        System.out.println("a-b= " + r);
+        //Произведение чисел
+        int p = a*b;
+        System.out.println("a*b =" + p);
+        //Деление чисел
+        double q = (double) a/b;
+        System.out.println("a/b =" +  q);
     }
 }
